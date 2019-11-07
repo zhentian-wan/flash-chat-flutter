0212bdbea647b05c92643c3d91094291556c5b56
zhentian-wan <answer881215@gmail.com>
2019-11-07 21:17:21 +0200

    override constant

--- lib/main.dart
+++ lib/main.dart
@@ -9,18 +9,11 @@ void main() => runApp(FlashChat());
 class FlashChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        theme: ThemeData.dark().copyWith(
-          textTheme: TextTheme(
-            body1: TextStyle(color: Colors.black54),
-          ),
-        ),
-        initialRoute: WelcomeScreen.id,
-        routes: {
-          WelcomeScreen.id: (context) => WelcomeScreen(),
-          RegistrationScreen.id: (context) => RegistrationScreen(),
-          LoginScreen.id: (context) => LoginScreen(),
-          ChatScreen.id: (context) => ChatScreen(),
-        });
+    return MaterialApp(initialRoute: WelcomeScreen.id, routes: {
+      WelcomeScreen.id: (context) => WelcomeScreen(),
+      RegistrationScreen.id: (context) => RegistrationScreen(),
+      LoginScreen.id: (context) => LoginScreen(),
+      ChatScreen.id: (context) => ChatScreen(),
+    });
   }
 }
